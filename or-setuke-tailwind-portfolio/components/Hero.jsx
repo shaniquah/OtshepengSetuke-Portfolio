aad1cd83e34d1a2c27dea7c77d5d8a7de16c63cd
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-09-27 23:36:43 +0200

    Update Hero.jsx

--- or-setuke-tailwind-portfolio/components/Hero.jsx
+++ or-setuke-tailwind-portfolio/components/Hero.jsx
@@ -1,9 +1,17 @@
 export default function Hero() {
   return (
     <section>
+<<<<<<< Updated upstream
       <h1 className="text-white">Hi, I'm Otshepeng!</h1>
       <p className="text-[#90afc5]">
         
+=======
+      <h1 className="text-white mb-4 text-6xl font-extrabold">Hi, I'm Otshepeng!</h1>
+      <p className="text-[#90afc5]">
+      I am an aspiring Junior Software Developer with experience in Full-Stack
+        JavaScript Engineering. I have experience in web development especially
+        frontend frameworks like ReactJS.
+>>>>>>> Stashed changes
       </p>
     </section>
   );
